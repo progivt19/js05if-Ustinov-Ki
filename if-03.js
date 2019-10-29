bd17dd2215fd098fe898205f0d0dca88ec035a16
Ustinov-Ki <54973177+Ustinov-Ki@users.noreply.github.com>
2019-10-29 09:03:05 +0900

    Update and rename if-x1.js to if-03.js

---
+++ if-03.js
@@ -0,0 +1,29 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+var max1 = 0
+var max2 = 0
+
+function ...(){
+    if (a > b){
+        max1 = a
+    }
+    else{
+        max1 = b
+}
+    if (c > d){
+        max2 = c
+}
+    else{
+        max2 = d
+}
+    if (max1 < max2){
+        return (max1)
+    }
+    else{
+        return (max2)
+    }
+}
+
+module.exports = ...;
